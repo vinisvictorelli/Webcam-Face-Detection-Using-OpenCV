78e44e75f9b24ace34d8099da453a03573263f06
Vinícius Soares Victorelli <58866162+vinisvictorelli@users.noreply.github.com>
2021-01-14 20:36:44 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import cv2
+import sys
+import datetime as dt
+from time import sleep
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+eyesCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(1)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    #Captura o video frame por frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    faces = faceCascade.detectMultiScale(gray)
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        faceROI = gray[y:y+h,x:x+w]
+        #-- In each face, detect eyes
+        eyes = eyesCascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
+    
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
